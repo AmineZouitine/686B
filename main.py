efe767fb5dd33f205a2d36841a4147cd07c4d47d
Amine Zouitine <amine.zouitine@qonto.com>
2024-05-16 18:25:59 +0200

    fix output

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+def convert(lst):
+    return ' '.join(map(str, lst))
+
 def bfs(shortCuts):
     max_size = len(shortCuts)
     mins = [float('inf')] * max_size 
@@ -16,8 +19,13 @@ def bfs(shortCuts):
             mins[shortcut_index] = current_energy + 1
             queue.append((shortcut_index, current_energy + 1))
 
+        if current_index - 1 >= 0 and current_energy + 1 < mins[current_index - 1]:
+            mins[current_index - 1] = current_energy + 1
+            queue.append((current_index - 1, current_energy + 1))
+
     return mins
 
 _ = input()
 shortCuts = list(map(int, input().split()))
-print(bfs(shortCuts))
+print(convert(bfs(shortCuts)))
+
